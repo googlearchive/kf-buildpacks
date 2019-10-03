1f31bfdcc3943e98d8148ba2e9d173a58f251107
Matthew Broomfield <mattysweeps@google.com>
2019-10-03 11:18:53 -0700

    Download a pack by release tag

--- hack/upload-buildpacks.sh
+++ hack/upload-buildpacks.sh
@@ -23,7 +23,7 @@ shift
 
 readonly version=${1:-latest}
 
-rootpath=$(cd $(dirname $0)/.. && pwd)
+rootpath=$(cd "$(dirname "$0")/.." && pwd)
 dir=${rootpath}/builder
 
 builder_config=${dir}/builder.toml
@@ -34,18 +34,30 @@ builder_image="${registry}/buildpack-builder:${version}"
 # paths to the buildpacks.
 temp_dir=$(mktemp -d)
 function finish {
-  rm -rf "$temp_dir"
+  rm -rf "${temp_dir}"
 }
 trap finish EXIT
-cp -r $rootpath/builder/* $temp_dir
+cp -r "$rootpath/builder"/* "$temp_dir"
 
 # For each CF buildpack that we have to build
 build_cf_buildpack(){
   echo "Building $1"
-  pushd $temp_dir/$1
+  pushd "$temp_dir/$1"
   [ -f "./ci/build.sh" ] && ./ci/build.sh
   [ -f "./scripts/build.sh" ] && ./scripts/build.sh
   popd
+}
+
+build_pack(){
+  PACK_VERSION=v0.2.1
+  PACK_SHA256=e0fc11016bf15f2d03197d1a6fd872f0524b47f53c6bd2731717b1d0b3c10f04
+  curl -LJo "${temp_dir}/pack.tar.gz" https://github.com/buildpack/pack/releases/download/${PACK_VERSION}/pack-${PACK_VERSION}-linux.tgz
+  tar xfz "${temp_dir}/pack.tar.gz" -C "${temp_dir}"
+  [ "$PACK_SHA256" = "$(sha256sum < "${temp_dir}/pack.tar.gz" | cut -d' ' -f1)" ]
+}
+
+run_pack(){
+  "${temp_dir}/pack" $@
 }
 
 build_cf_buildpack "archive-expanding-cnb"
@@ -59,19 +71,16 @@ build_cf_buildpack "tomcat-cnb"
 
 # Fill in our gcr.io registry for the builder
 builder_toml=$temp_dir/builder.toml
-cat $builder_toml | sed "s|REPLACE_WITH_REGISTRY|$registry|g" > $builder_toml.new
+sed "s|REPLACE_WITH_REGISTRY|$registry|g" < "$builder_toml" > "$builder_toml.new"
 
+build_pack
 echo "building builder from $builder_config..."
-pack create-builder $builder_image --builder-config $builder_toml.new
+run_pack create-builder "$builder_image" --builder-config "$builder_toml.new"
 
-if [ $publish == "true" ]; then
-    echo "Publishing these images..."
-    for image in "${builder_image}"; do
-      docker push ${image}
-    done
+if [ "$publish" == "true" ]; then
+    echo "Publishing ${builder_image}..."
+    docker push "${builder_image}"
 else
-    echo "To publish these images:"
-    for image in "${builder_image}"; do
-      echo "  docker push ${image}"
-    done
+    echo "To publish ${builder_image}:"
+    echo "  docker push ${builder_image}"
 fi
